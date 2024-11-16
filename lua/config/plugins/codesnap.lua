50ea8cd7069b2bcd5a6f030cada3dfb017c63c87
Connor van Spronssen <info@dobefu.com>
2024-11-16 15:00:02 +0100

    Sat Nov 16 15:00:02 CET 2024

--- lua/config/plugins/codesnap.lua
+++ lua/config/plugins/codesnap.lua
@@ -1,9 +1,16 @@
-require('codesnap').setup({
+local codesnap = require('codesnap')
+
+codesnap.setup({
   watermark = '',
+  bg_color = "#535c68",
   has_breadcrumbs = true,
-  has_line_number = true,
+  has_line_number = false,
   show_workspace = true,
   bg_x_padding = 0,
   bg_y_padding = 0,
   bg_padding = nil,
 })
+
+vim.api.nvim_create_user_command('Screenshot', function()
+  codesnap.copy_into_clipboard()
+end, { range = true })
