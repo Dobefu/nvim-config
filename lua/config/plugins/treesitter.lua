63a85fca17c327bff2cdcafaaffe4de3aaa98563
Connor van Spronssen <info@dobefu.com>
2025-01-09 07:13:56 +0100

    Thu Jan  9 07:13:56 CET 2025

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,37 +1,37 @@
 local treesitter_configs = require('nvim-treesitter.configs')
 
 treesitter_configs.setup({
-  ensure_installed = {
-    "html",
-    "php",
-    "javascript",
-    "typescript",
-    "c",
-    "lua",
-    "go",
-    "vue",
-    "tsx",
-  },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    disable = function(_, buf)
-      local max_filesize = 100 * 1024 -- 100 KB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+	ensure_installed = {
+		"html",
+		"php",
+		"javascript",
+		"typescript",
+		"c",
+		"lua",
+		"go",
+		"vue",
+		"tsx",
+	},
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		disable = function(_, buf)
+			local max_filesize = 100 * 1024 -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 
-      if ok and stats and stats.size > max_filesize then
-        return true
-      end
-    end,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true,
-  },
-  autotag = {
-    enable = true,
-  },
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
+		additional_vim_regex_highlighting = false,
+	},
+	indent = {
+		enable = true,
+	},
+	autotag = {
+		enable = true,
+	},
 })
 
 vim.treesitter.language.register('markdown', 'mdx')
