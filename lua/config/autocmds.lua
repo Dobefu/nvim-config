5231c1756fe180e0c1f97b26d2f12ad7f2b62b68
Connor van Spronssen <info@dobefu.com>
2025-01-09 07:14:22 +0100

    Thu Jan  9 07:14:22 CET 2025

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,13 +5,13 @@ vim.api.nvim_create_autocmd('bufWritePost', {
 })
 
 -- Custom settings for C code.
-vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
   pattern = '*.{c,h,s,asm,cpp}',
   callback = function() vim.cmd('set cindent noexpandtab tabstop=4 shiftwidth=4') end
 })
 
 -- Set PHP filetype for non-standard file types.
-vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
   pattern = '*.{module,theme,install,test,inc,profile,view}',
   callback = function() vim.cmd('set filetype=php') end
 })
