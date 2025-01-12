362251656c856b85207a226ac95bd13fdf74477e
Connor van Spronssen <info@dobefu.com>
2025-01-12 13:00:02 +0100

    Sun Jan 12 13:00:02 CET 2025

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -1,27 +1,28 @@
 local conform = require("conform")
 
 conform.setup({
-	formatters_by_ft = {
-		javascript = { "prettierd" },
-		typescript = { "prettierd" },
-		javascriptreact = { "prettierd" },
-		typescriptreact = { "prettierd" },
-		vue = { "prettierd" },
-		html = { "prettierd" },
-		go = { "goimports", "gofmt" },
-		twig = { "twig-cs-fixer" },
-		markdown = { "markdown-toc", "prettierd" },
-	},
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	},
+  formatters_by_ft = {
+    javascript = { "prettierd" },
+    typescript = { "prettierd" },
+    javascriptreact = { "prettierd" },
+    typescriptreact = { "prettierd" },
+    vue = { "prettierd" },
+    html = { "prettierd" },
+    go = { "goimports", "gofmt" },
+    twig = { "twig-cs-fixer" },
+    markdown = { "markdown-toc", "prettierd" },
+    groovy = { "npm-groovy-lint" },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
 })
 
 -- Format on save.
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		conform.format({ bufnr = args.buf })
-	end,
+  pattern = "*",
+  callback = function(args)
+    conform.format({ bufnr = args.buf })
+  end,
 })
