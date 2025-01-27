65e59249b2d71ce5b0d0edc82380252ff227bd1e
Connor van Spronssen <info@dobefu.com>
2025-01-27 05:29:11 +0100

    Mon Jan 27 05:29:11 CET 2025

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,48 +1,47 @@
 local builtin = require('telescope.builtin')
 local actions = require("telescope.actions")
-local config = require("telescope.config")
 
 local opts = {
-	hidden = true,
-	no_ignore = true,
-	no_ignore_parent = true,
+  hidden = true,
+  no_ignore = true,
+  no_ignore_parent = true,
 }
 
 vim.keymap.set('n', '<C-p>', function() builtin.find_files(opts) end, { desc = "Find files" })
 vim.keymap.set('n', '<C-l>', function() builtin.live_grep(opts) end, { desc = "Live grep" })
 
 require("telescope").setup({
-	defaults = {
-		prompt_prefix = " 🔍 ",
-		mappings = {
-			i = {
-				["<esc>"] = actions.close,
-				["<CR>"] = actions.select_tab,
-			},
-		},
-		sorting_strategy = 'ascending',
-		layout_config = {
-			prompt_position = 'top',
-		},
-		preview = {
-			treesitter = false,
-		},
-		file_ignore_patterns = {
-			"~$",
-			"undodir/",
-			".DS_Store",
-			".git/",
-			"swap/",
-			".swp",
-			"node_modules/",
-			"vendor/",
-			"tempformresults/",
-			"web/modules/custom/",
-			"web/themes/custom/",
-			"coverage/",
-			".next/",
-			".nuxt/",
-			".output/",
-		},
-	},
+  defaults = {
+    prompt_prefix = " 🔍 ",
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<CR>"] = actions.select_tab,
+      },
+    },
+    sorting_strategy = 'ascending',
+    layout_config = {
+      prompt_position = 'top',
+    },
+    preview = {
+      treesitter = false,
+    },
+    file_ignore_patterns = {
+      "~$",
+      "undodir/",
+      ".DS_Store",
+      ".git/",
+      "swap/",
+      ".swp",
+      "node_modules/",
+      "vendor/",
+      "tempformresults/",
+      "web/modules/custom/",
+      "web/themes/custom/",
+      "coverage/",
+      ".next/",
+      ".nuxt/",
+      ".output/",
+    },
+  },
 })
