7071aa545d30a579af52ab7309cf7a26004f8e2f
Connor van Spronssen <info@dobefu.com>
2025-01-27 06:00:02 +0100

    Mon Jan 27 06:00:02 CET 2025

--- lua/config/plugins/outline.lua
+++ lua/config/plugins/outline.lua
@@ -1,15 +1,15 @@
 local outline = require('outline')
 
 outline.setup({
-	outline_window = {
-		auto_close = true,
-	},
-	symbol_folding = {
-		autofold_depth = 1,
-		auto_unfold = {
-			hovered = true,
-		},
-	},
+  outline_window = {
+    auto_close = true,
+  },
+  symbol_folding = {
+    autofold_depth = 1,
+    auto_unfold = {
+      hovered = true,
+    },
+  },
 })
 
 vim.keymap.set("n", "<leader>o", ":Outline<CR>", { desc = "Toggle Outline" })
