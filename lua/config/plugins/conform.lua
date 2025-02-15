cc3f46dbb390420e2c83137f6698881fe7a991ce
Connor van Spronssen <info@dobefu.com>
2025-02-15 14:39:51 +0100

    Sat Feb 15 14:39:51 CET 2025

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -2,16 +2,17 @@ local conform = require("conform")
 
 conform.setup({
   formatters_by_ft = {
-    javascript = { "prettierd" },
-    typescript = { "prettierd" },
+    javascript      = { "prettierd" },
+    typescript      = { "prettierd" },
     javascriptreact = { "prettierd" },
     typescriptreact = { "prettierd" },
-    vue = { "prettierd" },
-    html = { "prettierd" },
-    go = { "goimports", "gofmt" },
-    twig = { "twig-cs-fixer" },
-    markdown = { "markdown-toc", "prettierd" },
-    groovy = { "npm-groovy-lint" },
+    vue             = { "prettierd" },
+    html            = { "prettierd" },
+    css             = { "prettierd" },
+    go              = { "goimports", "gofmt" },
+    twig            = { "twig-cs-fixer" },
+    markdown        = { "markdown-toc", "prettierd" },
+    groovy          = { "npm-groovy-lint" },
   },
   format_on_save = {
     timeout_ms = 500,
