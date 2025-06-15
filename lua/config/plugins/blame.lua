d3a39d57fcc3463d898fdef912477e54213ee7b7
Connor van Spronssen <info@dobefu.com>
2025-06-15 10:44:31 +0200

    Sun Jun 15 10:44:31 CEST 2025

--- lua/config/plugins/blame.lua
+++ lua/config/plugins/blame.lua
@@ -1,4 +1,4 @@
 local blame = require('blame')
 blame.setup({})
 
-vim.api.nvim_set_keymap('n', '<C-b>', ':BlameToggle<CR>', { desc = "Git Blame" })
+vim.api.nvim_set_keymap('n', '<C-b>', ':BlameToggle<CR>', { desc = 'Git Blame' })
