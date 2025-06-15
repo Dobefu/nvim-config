e22a277b43c0c9cd9fdf44c5e3d12e97c88a3741
Connor van Spronssen <info@dobefu.com>
2025-06-15 10:46:20 +0200

    Sun Jun 15 10:46:20 CEST 2025

--- lua/config/plugins/noice.lua
+++ lua/config/plugins/noice.lua
@@ -1,40 +1,40 @@
-local noice = require("noice")
+local noice = require('noice')
 
 noice.setup({
-  cmdline = {
-    view = "cmdline",
-  },
-  messages = {
-    view = "mini",
-  },
-  hover = {
-    enabled = false,
-  },
-  lsp = {
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-    },
-    signature = {
-      enabled = false,
-    },
-  },
-  presets = {
-    bottom_search = true,
-    command_palette = true,
-    long_message_to_split = true,
-    inc_rename = false,
-    lsp_doc_border = false,
-  },
-  routes = {
-    { filter = { event = "notify", find = "SonarQube language server is ready" },    view = "mini" },
-    { filter = { event = "msg_show", kind = "", find = "written" },                  view = "mini" },
-    { filter = { event = "msg_show", kind = "", find = "Already at oldest change" }, view = "mini" },
-    { filter = { event = "msg_show", kind = "", find = "change; before" },           view = "mini" },
-    { filter = { event = "msg_show", kind = "", find = "changes; before" },          view = "mini" },
-    { filter = { event = "msg_show", kind = "", find = "change; after" },            view = "mini" },
-    { filter = { event = "msg_show", kind = "", find = "changes; after" },           view = "mini" },
-    { filter = { event = "msg_show", kind = "emsg", find = "Pattern not found" },    view = "mini" },
-  },
+	cmdline = {
+		view = 'cmdline',
+	},
+	messages = {
+		view = 'mini',
+	},
+	hover = {
+		enabled = false,
+	},
+	lsp = {
+		override = {
+			['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+			['vim.lsp.util.stylize_markdown'] = true,
+			['cmp.entry.get_documentation'] = true,
+		},
+		signature = {
+			enabled = false,
+		},
+	},
+	presets = {
+		bottom_search = true,
+		command_palette = true,
+		long_message_to_split = true,
+		inc_rename = false,
+		lsp_doc_border = false,
+	},
+	routes = {
+		{ filter = { event = 'notify', find = 'SonarQube language server is ready' },    view = 'mini' },
+		{ filter = { event = 'msg_show', kind = '', find = 'written' },                  view = 'mini' },
+		{ filter = { event = 'msg_show', kind = '', find = 'Already at oldest change' }, view = 'mini' },
+		{ filter = { event = 'msg_show', kind = '', find = 'change; before' },           view = 'mini' },
+		{ filter = { event = 'msg_show', kind = '', find = 'changes; before' },          view = 'mini' },
+		{ filter = { event = 'msg_show', kind = '', find = 'change; after' },            view = 'mini' },
+		{ filter = { event = 'msg_show', kind = '', find = 'changes; after' },           view = 'mini' },
+		{ filter = { event = 'msg_show', kind = 'emsg', find = 'Pattern not found' },    view = 'mini' },
+	},
 })
