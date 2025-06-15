fa858a000c240cc5ace2195b9610b7206a4d3ffb
Connor van Spronssen <info@dobefu.com>
2025-06-15 12:30:46 +0200

    Sun Jun 15 12:30:46 CEST 2025

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -207,24 +207,36 @@ vim.lsp.config('denols', {
 
 vim.lsp.enable('lua_ls')
 vim.lsp.config('lua_ls', {
-  capabilities = vim.lsp.protocol.make_client_capabilities(),
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = {
-          'vim'
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+          path ~= vim.fn.stdpath('config')
+          and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT',
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
         },
-        disable = { 'missing-fields' },
       },
-      telemetry = { enable = false },
       workspace = {
+        checkThirdParty = false,
         library = {
-          '$VIMRUNTIME/lua/vim',
-          '$HOME/.local/share/nvim/site/pack/packer/start',
-        },
-      },
-    },
-  },
+          vim.env.VIMRUNTIME
+        }
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
 })
 
 sonarlint.setup({
