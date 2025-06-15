2ec8deef2e1e95713e4bc1d393205ec402b56db9
Connor van Spronssen <info@dobefu.com>
2025-06-15 10:44:21 +0200

    Sun Jun 15 10:44:21 CEST 2025

--- lua/config/plugins/better-diagnostic-virtual-text.lua
+++ lua/config/plugins/better-diagnostic-virtual-text.lua
@@ -1,15 +1,15 @@
 local better_diagnostic_virtual_text = require('better-diagnostic-virtual-text')
 
 better_diagnostic_virtual_text.setup({
-  ui = {
-    wrap_line_after = false,
-    left_kept_space = 3,
-    right_kept_space = 3,
-    arrow = "  ",
-    up_arrow = "  ",
-    down_arrow = "  ",
-    above = false,
-  },
-  priority = 2003,
-  inline = true,
+	ui = {
+		wrap_line_after = false,
+		left_kept_space = 3,
+		right_kept_space = 3,
+		arrow = '  ',
+		up_arrow = '  ',
+		down_arrow = '  ',
+		above = false,
+	},
+	priority = 2003,
+	inline = true,
 })
