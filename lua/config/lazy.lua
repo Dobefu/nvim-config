77a80dee20f802c08a5df4fb7ceb08778b13d068
Connor van Spronssen <info@dobefu.com>
2025-06-15 15:02:18 +0200

    Sun Jun 15 15:02:18 CEST 2025

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -52,16 +52,14 @@ require('lazy').setup({
       'hrsh7th/nvim-cmp',
       event = 'InsertEnter',
       dependencies = {
-        'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lua',
+        'saadparwaiz1/cmp_luasnip',
       },
     },
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-nvim-lua',
-    'saadparwaiz1/cmp_luasnip',
     {
       'ray-x/lsp_signature.nvim',
       event = 'InsertEnter',
@@ -70,6 +68,12 @@ require('lazy').setup({
       'L3MON4D3/LuaSnip',
       version = 'v2.*',
       build = 'make install_jsregexp',
+      dependencies = {
+        'rafamadriz/friendly-snippets',
+        config = function()
+          require('luasnip.loaders.from_vscode').lazy_load()
+        end,
+      },
     },
     'folke/neoconf.nvim',
     'https://gitlab.com/schrieveslaach/sonarlint.nvim',
