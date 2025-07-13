f394afc78f161d61e02404e89518ba2bce658ef1
Connor van Spronssen <info@dobefu.com>
2025-07-13 17:08:39 +0200

    Sun Jul 13 17:08:39 CEST 2025

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -76,32 +76,37 @@ vim.lsp.config('gopls', {
 })
 
 vim.lsp.enable('ts_ls')
-vim.lsp.config('ts_ls', {
-  single_file_support = false,
-  init_options = {
-    plugins = { {
-      name = '@vue/typescript-plugin',
-      location = vim.fn.expand(
-        '$MASON/packages/vue-language-server/node_modules/@vue/language-server'
-      ),
-      languages = { 'javascript', 'typescript', 'vue' },
-    } },
-  },
-  settings = {
-    typescript = {
-      inlayHints = {
-        includeInlayParameterNameHints = 'none',
-        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-        includeInlayFunctionParameterTypeHints = false,
-        includeInlayVariableTypeHints = false,
-        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-        includeInlayPropertyDeclarationTypeHints = false,
-        includeInlayFunctionLikeReturnTypeHints = false,
-        includeInlayEnumMemberValueHints = false,
+vim.lsp.config(
+  'ts_ls',
+  {
+    single_file_support = false,
+    init_options = {
+      plugins = {
+        {
+          name = '@vue/typescript-plugin',
+          location = vim.fn.expand(
+            '$MASON/packages/vue-language-server/node_modules/@vue/language-server'
+          ),
+          languages = { 'javascript', 'typescript', 'vue' },
+        },
       },
     },
-  },
-})
+    settings = {
+      typescript = {
+        inlayHints = {
+          includeInlayParameterNameHints = 'none',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayFunctionParameterTypeHints = false,
+          includeInlayVariableTypeHints = false,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+          includeInlayPropertyDeclarationTypeHints = false,
+          includeInlayFunctionLikeReturnTypeHints = false,
+          includeInlayEnumMemberValueHints = false,
+        },
+      },
+    },
+  }
+)
 
 vim.lsp.enable('vue_ls')
 vim.lsp.config('vue_ls', {
