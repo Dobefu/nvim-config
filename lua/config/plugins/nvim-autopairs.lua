2ef60a0e709861cbaecbb9eeef4b96d1d2414660
Connor van Spronssen <info@dobefu.com>
2025-07-19 13:26:10 +0200

    Sat Jul 19 13:26:10 CEST 2025

--- lua/config/plugins/nvim-autopairs.lua
+++ lua/config/plugins/nvim-autopairs.lua
@@ -1,5 +1,6 @@
 local autopairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
+local ts_conds = require('nvim-autopairs.ts-conds')
 
 autopairs.setup({
   check_ts = true,
@@ -10,11 +11,7 @@ autopairs.setup({
   },
 })
 
-local ts_conds = require('nvim-autopairs.ts-conds')
-
 autopairs.add_rules({
-  Rule('%', '%', 'lua')
-      :with_pair(ts_conds.is_ts_node({ 'string', 'comment' })),
-  Rule('$', '$', 'lua')
-      :with_pair(ts_conds.is_not_ts_node({ 'function' }))
+  Rule('%', '%', 'lua'):with_pair(ts_conds.is_ts_node({ 'string', 'comment' })),
+  Rule('$', '$', 'lua'):with_pair(ts_conds.is_not_ts_node({ 'function' }))
 })
