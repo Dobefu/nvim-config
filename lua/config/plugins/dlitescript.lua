6b32bc9ada453c470d137706ba3cba38e7ce6d32
Connor van Spronssen <info@dobefu.com>
2025-08-19 12:00:02 +0200

    Tue Aug 19 12:00:02 CEST 2025

--- lua/config/plugins/dlitescript.lua
+++ lua/config/plugins/dlitescript.lua
@@ -1,3 +1,24 @@
 local dlitescript = require('nvim-dlitescript')
 
-dlitescript.setup({})
+dlitescript.setup({
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
+  fold = {
+    enable = false,
+  },
+  auto_install = true,
+  lsp = {
+    enable = true,
+    cmd = {
+      'sh',
+      '-c',
+      'cd ~/Projects/Web/Golang/DLiteScript && go run . lsp --stdio',
+    },
+    root_markers = { '.git' },
+    settings = {},
+  },
+})
